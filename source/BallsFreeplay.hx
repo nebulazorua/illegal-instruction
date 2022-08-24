a2da7ce4bdd60c9086ff6a59d41c14a5de42efe6
jackexe123 <sawyercampbell777@outlook.com>
2022-08-23 21:25:47 -0400

    to the void i retturn

--- source/BallsFreeplay.hx
+++ source/BallsFreeplay.hx
@@ -27,8 +27,9 @@ class BallsFreeplay extends MusicBeatState
 {
     var songs:Array<String> = ['breakout', 'soulless-endeavors', 'final-frontier', 'my-horizon', 'our-horizon', 'found-you', 'malediction', 'long-sky', 'hedge', 'manual-blast', 'endless',];
     private var curSelected:Int = 0;
+    private var grpImages:FlxTypedGroup<MenuItemAgainFuckYou>;
 
-    var selectorSprite:FlxSprite;
+    var selectorSprite:MenuItemAgainFuckYou;
     var imageName:String;
 
     override function create()
@@ -36,10 +37,64 @@ class BallsFreeplay extends MusicBeatState
         Paths.clearStoredMemory();
 		Paths.clearUnusedMemory();
 
+        grpImages = new FlxTypedGroup<MenuItemAgainFuckYou>();
+		add(grpImages);
+
         for (i in 0...songs.length){
             imageName = 'freeplay/' + songs[i];
-            selectorSprite = new FlxSprite(800 * i, 0).loadGraphic(Paths.image(imageName));
-            add(selectorSprite);
+            if(songs[i] == 'manual-blast' || songs[i] == 'hedge'){
+                imageName = 'freeplay/placeholder';
+            }
+            selectorSprite = new MenuItemAgainFuckYou(800 * i, 0, imageName);
+            selectorSprite.ID = i;
+            grpImages.add(selectorSprite);
         }
+    }
+    override function update(elapsed:Float)
+    {
+        var upP = controls.UI_UP_P;
+		var downP = controls.UI_DOWN_P;
+		var accepted = controls.ACCEPT;
+		var space = FlxG.keys.justPressed.SPACE;
+
+        selectorSprite.updateAgain(elapsed);
+
+        if(controls.UI_RIGHT_P)
+            changeSelection(1);
+        if(controls.UI_LEFT_P)
+            changeSelection(-1);
+        if(accepted){
+            var songLowercase:String = Paths.formatToSongPath(songs[curSelected]);
+
+            PlayState.SONG = Song.loadFromJson(songLowercase + '-hard', songLowercase);
+			PlayState.isStoryMode = false;
+
+            LoadingState.loadAndSwitchState(new PlayState());
+        }
+    }
+    function changeSelection(change:Int){
+        curSelected += change;
+
+        if (curSelected < 0)
+			curSelected = songs.length - 1;
+		if (curSelected >= songs.length)
+			curSelected = 0;
+
+		var bullShit:Int = 0;
+
+        for (item in grpImages.members)
+            {
+                item.ID = bullShit - curSelected;
+                bullShit++;
+    
+                item.alpha = 0.6;
+                item.newX = 800;
+                if (item.ID == 0)
+                {
+                    item.alpha = 1;
+                    item.newX = 0;
+                    trace("BALLS");
+                }
+            }
     }
 }
---
+++ source/MenuItemAgainFuckYou.hx
@@ -0,0 +1,39 @@
+package;
+
+#if desktop
+import Discord.DiscordClient;
+#end
+import editors.ChartingState;
+import flash.text.TextField;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.addons.display.FlxGridOverlay;
+import flixel.addons.transition.FlxTransitionableState;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.math.FlxMath;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import flixel.tweens.FlxTween;
+import lime.utils.Assets;
+import flixel.system.FlxSound;
+import openfl.utils.Assets as OpenFlAssets;
+#if MODS_ALLOWED
+import sys.FileSystem;
+#end
+
+using StringTools;
+
+class MenuItemAgainFuckYou extends FlxSprite
+{
+    public var newX:Float = 0;
+
+    public function new(x:Float, y:Float, yeah:String){
+        super(x, y);
+        loadGraphic(Paths.image(yeah));
+    }
+
+    public function updateAgain(elapsed:Float) // FOR SOME REASON NORMAL UPDATE BROKE SO IDFKKKKK
+    {
+        x = FlxMath.lerp(x, newX, CoolUtil.boundTo(elapsed * 10.2, 0, 1));
+    }
+}
